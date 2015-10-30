1cee4b8578abf5c2511c9739ed89cc4a6b24785e
zachstratton <zach.stratton@ymail.com>
2015-10-30 21:50:30 +0100

    origin

---
+++ Assets/Scripts/Character Classes/Abilities.cs
@@ -0,0 +1,23 @@
+﻿public class Abilities : ModifiedStat
+{
+    private bool _known;
+
+    public Abilities()
+    {
+        _known = false;
+        ExpToLevel = 25;
+        LevelModifier = 1.1f;
+    }
+
+    public bool Known
+    {
+        get { return _known; }
+        set { _known = value; }
+    }
+}
+
+public enum SkillName
+{
+    Putabilityshithere,
+    andhere
+}
---
+++ Assets/Scripts/Character Classes/Stat.cs
@@ -0,0 +1,20 @@
+﻿public class Stat : BaseStat
+{
+    public Stat()
+    {
+        ExpToLevel = 50;
+        LevelModifier = 1.05f;
+    }
+}
+
+public enum StatName
+{
+    Accuracy,
+    Defense,
+    Vitality,
+    Intellect,
+    Dexterity,
+    Strength,
+    Endurance,
+    Crit
+}
